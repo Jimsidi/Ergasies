39c6e488cb39c6b447b0e11dc89515a042c19287
Jim Sidiropoulos <91787065+Jimsidi@users.noreply.github.com>
2021-10-07 12:23:37 +0300

    Update ergasia9.py

--- ergasia9.py
+++ ergasia9.py
@@ -7,16 +7,12 @@ allowed = set(ascii_letters + ' ')
 answer = ''.join(l for l in textfile if l in allowed)
 
 ASCII_values = [ord(character) for character in answer]
-#print(ASCII_values)
 
 sentence = "".join(map(chr,  ASCII_values)).split()
-#print(sentence)
 
 sentence = [word for word in sentence if not ord(word[-1]) % 2]
-#print(sentence)
 
 flist = [ord(ele) for sub in sentence for ele in sub]
-#print(flist)
 
 maximum = max(flist)
 minimum = min(flist)
